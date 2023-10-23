2b6dff68ad023e4875fca272b44b7a203299b9c7
NasimALJr <148753134+NasimALJr@users.noreply.github.com>
2023-10-23 14:10:44 +0600

    Add files via upload
    Problem No: 4A
    Problem Name: Watermelon

---
+++ CF_Watermelon.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    int w;
+    scanf("%d",&w);
+    if(w==2 || w%2==1)
+    {
+        printf("No\n");
+    }
+    else
+    {
+        printf("Yes\n");
+    }
+    return 0;
+}
+
